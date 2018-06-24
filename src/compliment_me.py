373ac3a84a86893a6a254abe31fbd5bba069abd4
CJ Williams <chriswilliams4400@gmail.com>
2018-06-24 21:19:11 +0100

    Initial setup

---
+++ src/compliment_me.py
@@ -0,0 +1,143 @@
+from __future__ import print_function
+import urllib2
+
+
+# Response Building Helpers. Courtesy of Amazon.
+
+def build_speechlet_response(title, output, reprompt_text, should_end_session):
+    return {
+        'outputSpeech': {
+            'type': 'PlainText',
+            'text': output
+        },
+        'card': {
+            'type': 'Simple',
+            'title': title,
+            'content': output
+        },
+        'reprompt': {
+            'outputSpeech': {
+                'type': 'PlainText',
+                'text': reprompt_text
+            }
+        },
+        'shouldEndSession': should_end_session
+    }
+
+
+def build_response(session_attributes, speechlet_response):
+    return {
+        'version': '1.0',
+        'sessionAttributes': session_attributes,
+        'response': speechlet_response
+    }
+
+# Control Functions
+
+def do_welcome():
+    session_attributes = {}
+    card_title = "Welcome!"
+    speech_output = "Welcome! " \
+                    "Feel free to ask me for a compliment!"
+    reprompt_text = "Feel free to ask me for a compliment!"
+    should_end_session = False
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+
+def give_compliment():
+    raw = urllib2.urlopen("https://compliment-api.herokuapp.com/").read()
+    raw = raw[:-1]
+    session_attributes = {}
+    card_title = "Here's a compliment"
+    speech_output = raw 
+    reprompt_text = "Sorry I didn't get that!"
+    should_end_session = True
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+
+
+def help_response():
+    session_attributes = {}
+    card_title = "What can I do?"
+    speech_output = "You can ask me for a compliment. I like giving them!"
+    reprompt_text = "Feel free to ask me for a compliment"
+    should_end_session = False
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+
+
+def handle_session_end_request():
+    card_title = "Session Ended"
+    speech_output = "Have a great day!" \
+    # Setting this to true ends the session and exits the skill.
+    should_end_session = True
+    return build_response({}, build_speechlet_response(
+        card_title, speech_output, None, should_end_session))
+
+
+# Events
+
+
+def on_session_started(session_started_request, session):
+    print("on_session_started requestId=" + session_started_request['requestId']
+          + ", sessionId=" + session['sessionId'])
+
+
+def on_launch(launch_request, session):
+    print("on_launch requestId=" + launch_request['requestId'] +
+          ", sessionId=" + session['sessionId'])
+    # Dispatch to your skill's launch
+    return do_welcome()
+
+
+def on_intent(intent_request, session):
+    print("on_intent requestId=" + intent_request['requestId'] +
+          ", sessionId=" + session['sessionId'])
+
+    intent = intent_request['intent']
+    intent_name = intent_request['intent']['name']
+
+    # Dispatch to control functions
+    if intent_name == "ComplimentMe":
+        return give_compliment()
+    elif intent_name == "AMAZON.HelpIntent":
+        return help_response()
+    elif intent_name == "AMAZON.CancelIntent" or intent_name == "AMAZON.StopIntent":
+        return handle_session_end_request()
+    else:
+        raise ValueError("Invalid intent")
+
+
+def on_session_ended(session_ended_request, session):
+    """ Called when the user ends the session.
+
+    Is not called when the skill returns should_end_session=true
+    """
+    print("on_session_ended requestId=" + session_ended_request['requestId'] +
+          ", sessionId=" + session['sessionId'])
+
+
+# Main handler to deal with incoming JSON requests and trigger the right event
+
+
+def lambda_handler(event, context):
+
+    print("event.session.application.applicationId=" +
+          event['session']['application']['applicationId'])
+
+    if (event['session']['application']['applicationId'] !=
+            "amzn1.ask.skill.3eab4f46-15ac-4232-979f-f1bf713ba442"):
+        raise ValueError("Invalid Application ID")
+
+    if event['session']['new']:
+        on_session_started({'requestId': event['request']['requestId']},
+                           event['session'])
+
+    if event['request']['type'] == "LaunchRequest":
+        return on_launch(event['request'], event['session'])
+        print ("Done")
+    elif event['request']['type'] == "IntentRequest":
+        return on_intent(event['request'], event['session'])
+    elif event['request']['type'] == "SessionEndedRequest":
+        return on_session_ended(event['request'], event['session'])
+
